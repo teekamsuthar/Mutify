ea97e8091661a39bd3dd3e127bf18e1b79a5497f
teekamsuthar <teekam.suthar1@gmail.com>
2020-08-30 14:22:48 +0530

    initial commit

--- app/src/main/java/live/teekamsuthar/mutify/MainActivity.java
+++ app/src/main/java/live/teekamsuthar/mutify/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
 //        FloatingActionButton fab = findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
 //            @Override
